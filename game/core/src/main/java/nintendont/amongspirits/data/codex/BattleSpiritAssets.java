2456a5bd2000ce01173ed13e113dd850fcc73f0c
Paulo Troncone <Ptroncone205@users.noreply.github.com>
2026-01-20 10:17:59 -0400

    musica completa

--- game/core/src/main/java/nintendont/amongspirits/data/codex/BattleSpiritAssets.java
+++ game/core/src/main/java/nintendont/amongspirits/data/codex/BattleSpiritAssets.java
@@ -4,7 +4,7 @@ import com.badlogic.gdx.assets.AssetDescriptor;
 import com.badlogic.gdx.graphics.Texture;
 
 public class BattleSpiritAssets {
-    public static final AssetDescriptor<Texture> WU_ZETIAN = new AssetDescriptor<>("gokuprueba2.png", Texture.class);
+    public static final AssetDescriptor<Texture> WU_ZETIAN = new AssetDescriptor<>("sprites/spiritsHuman/phoenyx.png", Texture.class);
     public static final AssetDescriptor<Texture> MALE_LION = new AssetDescriptor<>("gokuprueba2.png", Texture.class);
     public static final AssetDescriptor<Texture> FEMALE_LION = new AssetDescriptor<>("gokuprueba2.png", Texture.class);
     public static final AssetDescriptor<Texture> MALE_DEER = new AssetDescriptor<>("sprites/spiritsHuman/maleDeer.png", Texture.class);
